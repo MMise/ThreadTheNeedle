88a5ddd75a8a9f7793d0b7ef63284f694b07f491
komuvill <V-V.Komulainen@hotmail.com>
2018-02-06 11:20:52 +0200

    Aloitussijainnit muutettu dynaamiseksi
    Langan ja neulan pitäisi nyt piirtyä aina samaan kohtaan suhteessa puhelimen näytön kokoon

--- main/java/neula/sovellus/NeedleView.java
+++ main/java/neula/sovellus/NeedleView.java
@@ -13,8 +13,8 @@ public class NeedleView extends View implements Runnable{
     int canvasW;
     int canvasH;
 
-    Lanka lanka = new Lanka(200,300);
-    Ball neula = new Ball(100,100,Color.BLUE);
+    Lanka lanka = new Lanka(0,0);
+    Ball neula = new Ball(0,0,Color.BLUE);
 
     Thread animationThread = null;
     boolean threadMustBeExecuted = false;
@@ -22,14 +22,19 @@ public class NeedleView extends View implements Runnable{
     int randomSeed = 0;
     Random generator = new Random();
     long newPosition = 0;
+    boolean firstCycle = true; //Käytetään neulan ja langan aloitussijainnin asettamiseen ensimmäisellä ohjelman suoritussyklillä
 
     public void onSizeChanged( int current_width_of_this_view,
                                int current_height_of_this_view,
                                int old_width_of_this_view,
                                int old_height_of_this_view )
     {
-        lanka.moveToPosition(150, 250);
-        neula.move_to_position(700, 250);
+        /*
+        System.out.println("canvasW: " + canvasW + " CanvasH: " + canvasH);
+        lanka.moveToPosition((canvasW / 2),  canvasH); // lanka piirtyy näytön leveyssuunnassa keskelle
+        neula.move_to_position(canvasW / 2, canvasH / 3);
+        System.out.println("NeulaX: " + neula.ball_center_point_x + " neulaY: " + neula.ball_center_point_y);
+        */
     }
 
     public NeedleView(Context context, @Nullable AttributeSet attrs) {
@@ -39,7 +44,8 @@ public class NeedleView extends View implements Runnable{
     public void getValues(float y, float x){
         int xx = (int) (x*0.7),
             yy = (int) (y*0.7);
-        System.out.println(neula.ball_center_point_x);
+
+
         if(xx > 0){
             if(neula.ball_center_point_x > 0){
                 neula.ball_center_point_x -= xx;
@@ -57,7 +63,7 @@ public class NeedleView extends View implements Runnable{
         }
         else if(yy < 0){
             if(lanka.startY > 0){
-                 lanka.startY += yy;
+                lanka.startY += yy;
             }
         }
         invalidate();
@@ -66,8 +72,15 @@ public class NeedleView extends View implements Runnable{
     protected void onDraw(Canvas canvas) {
         canvasW = canvas.getWidth();
         canvasH = canvas.getHeight();
-        lanka.draw(canvas);
-        neula.draw(canvas);
+        if(firstCycle){
+            lanka.moveToPosition(canvasW / 2,  (int) (canvasH / 1.5));
+            neula.move_to_position(canvasW / 2, canvasH / 4);
+            firstCycle = false;
+        }else{
+            lanka.draw(canvas);
+            neula.draw(canvas);
+        }
+
     }
 
     public void startThread(){
@@ -76,7 +89,6 @@ public class NeedleView extends View implements Runnable{
             threadMustBeExecuted = true;
             animationThread.start();
         }
-        System.out.println("Thread started!");
     }
 
     public void stopThread(){
@@ -85,26 +97,25 @@ public class NeedleView extends View implements Runnable{
             threadMustBeExecuted = false;
             animationThread = null;
         }
-        System.out.println("Thread stopped!");
     }
 
 
     @Override
     public void run() {
-        System.out.println("The thread is running my dudes!");
         while(threadMustBeExecuted){
 
             ticksSinceStart++;
             //Arvotaan langan sijainti
             randomSeed = generator.nextInt((1800 - 200 + 1) + 200);
-            newPosition = (long) (Math.sin(ticksSinceStart) + (randomSeed/100));
-
-            lanka.moveToPosition(250 + (int) newPosition, lanka.getY() );
+            newPosition = (long) (Math.sin(ticksSinceStart) * (randomSeed/100));
+            //Langan Y-sijainti ei muutu, tärinää lisätään X-sijainnille. CanvasW / 2 on langan X-sijainti aloitushetkellä
+            lanka.moveToPosition((canvasW / 2 + (int) newPosition), lanka.getY());
 
             //arvotaan neulan sijainti
             randomSeed = generator.nextInt((1300 - 700 + 1) + 700);
-            newPosition = (long) (Math.sin(ticksSinceStart) * (randomSeed/100 - 1301/200));
-            neula.move_to_position(neula.get_ball_center_point_x(), neula.get_ball_center_point_y() + (int) newPosition);
+            newPosition = (long) (Math.sin(ticksSinceStart) * (randomSeed/100));
+            //Neulan X-sijainti ei muutu, tärinää lisätään Y-sijainnille. CanvasH / 4 on neulan Y-sijainti aloitushetkellä
+            neula.move_to_position(neula.get_ball_center_point_x(), (canvasH / 4) + (int) newPosition);
             try{
                 animationThread.sleep(10);
             }catch(InterruptedException ex){
@@ -113,6 +124,5 @@ public class NeedleView extends View implements Runnable{
 
             postInvalidate();
         }
-        System.out.println("The thread has stopped my dudes!");
     }
 }
